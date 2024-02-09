eb21b4ee27eaa41ab44b287b931bbf6d41b3d16b
0KABE <RIN.OKAB3@GMAIL.COM>
2024-02-10 02:26:57 +0800

    Auto backup configs when calling export_to_host.sh

--- export_to_host.sh
+++ export_to_host.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+BACKUP_DIR="backup-$(date)"
+echo "backup config files in '$BACKUP_DIR/'"
+
+./sync_to_repo.sh "$BACKUP_DIR"
+
 git submodule update --init config/alacritty/themes
 cp .zshrc ~/.zshrc
 cp -r config/* ~/.config/
--- sync_to_repo.sh
+++ sync_to_repo.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-mkdir -p config
+BASE_DIR="${1:-.}"
+echo "copy configs => $BASE_DIR/"
 
-cp ~/.zshrc ./.zshrc
-
-cp -r ~/.config/alacritty ./config
-cp -r ~/.config/zellij ./config
+mkdir -p "$BASE_DIR/config"
+cp ~/.zshrc "$BASE_DIR/.zshrc"
+cp -r ~/.config/alacritty "$BASE_DIR/config"
+cp -r ~/.config/zellij "$BASE_DIR/config"
